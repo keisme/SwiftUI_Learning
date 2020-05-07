d5d8b2b3c53a2faaebe54981013a0fcc674f08f1
keisme <1822119568@qq.com>
2020-05-07 18:34:11 +0800

    init

---
+++ 100DaysOfSwiftUI/WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,85 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Bruce on 2020/5/7.
+//  Copyright © 2020 JFF147. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  @State private var tapCount = 0
+  @State private var name = ""
+  @State private var selectedStudent = 0
+  @State private var checkAmount = ""
+  @State private var numberOfPeople = 2
+  @State private var tipPercentage = 2
+  private let students = ["Harry", "Hermione", "Ron"]
+  private let tipPercentages = [10, 15, 20, 25, 0]
+  
+  var body: some View {
+    NavigationView {
+      Form {
+        Section {
+          Text("Hello, World!")
+        }
+        
+        Picker("Select your student", selection: $selectedStudent) {
+          ForEach(0..<students.count) {
+            Text(self.students[$0])
+          }
+        }
+        Text("You chose: Student # \(students[selectedStudent])")
+        
+        Section {
+          Button("Tap Count: \(tapCount)") {
+            self.tapCount += 1
+          }
+        }
+        
+        Section {
+          TextField("Enter you name", text: $name)
+          Text("Your name is \(name)")
+        }
+        
+        Section {
+          TextField("Amount", text: $checkAmount)
+            .keyboardType(.decimalPad)
+          Text("$\(checkAmount)")
+          Picker("Number of people", selection: $numberOfPeople) {
+            ForEach(2..<100) {
+              Text("\($0) people")
+            }
+          }
+        }
+        
+        Section(header: Text("How much tip do you want to leave?")) {
+          Picker("Tip percentage", selection: $tipPercentage) {
+            ForEach(0 ..< tipPercentages.count) {
+              Text("\(self.tipPercentages[$0])%")
+            }
+          }
+          .pickerStyle(SegmentedPickerStyle())
+        }
+      }
+      .navigationBarTitle("WeSplit")
+    }
+  }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    Group {
+      ContentView()
+        .previewDevice("iPhone XS")
+        .previewDisplayName("iPhone XS")
+      
+      ContentView()
+        .previewDevice("iPhone SE")
+        .previewDisplayName("iPhone SE")
+    }
+  }
+}
+#endif
