4ccbca1deaeaba3655239a356a7d1ccc8c17d66c
keisme <1822119568@qq.com>
2020-05-09 18:21:33 +0800

    add BetterRest

---
+++ 100DaysOfSwiftUI/BetterRest/BetterRest/ContentView.swift
@@ -0,0 +1,59 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by Bruce on 2020/5/9.
+//  Copyright © 2020 JFF147. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  @State private var wakeUp = Date()
+  @State private var sleepAmount = 8.0
+  @State private var coffeeAmount = 1
+  
+  var body: some View {
+    NavigationView {
+      VStack {
+        Text("When do you want to wake up?")
+          .font(.headline)
+
+        DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+            .labelsHidden()
+        
+        Text("Desired amount of sleep")
+            .font(.headline)
+
+        Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
+            Text("\(sleepAmount, specifier: "%g") hours")
+        }
+        
+        Text("Daily coffee intake")
+            .font(.headline)
+
+        Stepper(value: $coffeeAmount, in: 1...20) {
+            if coffeeAmount == 1 {
+                Text("1 cup")
+            } else {
+                Text("\(coffeeAmount) cups")
+            }
+        }
+      }
+    .navigationBarTitle("Better Rest")
+      .navigationBarItems(trailing: Button(action: calculateBedtime) {
+        Text("Calculate")
+      })
+    }
+  }
+  
+  func calculateBedtime() {
+    
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  }
+}
