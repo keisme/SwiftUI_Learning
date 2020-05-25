98970e927c6c43875fbcce7db03d1a816eb5357b
keisme <1822119568@qq.com>
2020-05-25 23:27:01 +0800

    update HotProspects

--- 100DaysOfSwiftUI/HotProspects/HotProspects/MeView.swift
+++ 100DaysOfSwiftUI/HotProspects/HotProspects/MeView.swift
@@ -7,15 +7,52 @@
 //
 
 import SwiftUI
+import CoreImage.CIFilterBuiltins
 
 struct MeView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  @State private var name = "Bruce"
+  @State private var emailAddress = "keisme3.0@gmail.com"
+  let context = CIContext()
+  let filter = CIFilter.qrCodeGenerator()
+  
+  var body: some View {
+    VStack {
+      TextField("Name", text: $name)
+        .textContentType(.name)
+        .font(.title)
+        .padding(.horizontal)
+      
+      TextField("Email address", text: $emailAddress)
+        .textContentType(.emailAddress)
+        .font(.title)
+        .padding([.horizontal, .bottom])
+      
+      Spacer()
+      
+      Image(uiImage: generateQRCode(from: "\(name)\n\(emailAddress)"))
+        .interpolation(.none)
+        .resizable()
+        .scaledToFit()
+        .frame(width: 200, height: 200)
     }
+    .navigationBarTitle("Bruce")
+  }
+  
+  func generateQRCode(from string: String) -> UIImage {
+    let data = Data(string.utf8)
+    filter.setValue(data, forKey: "inputMessage")
+    
+    if let outputImage = filter.outputImage {
+      if let cgImg = context.createCGImage(outputImage, from: outputImage.extent) {
+        return UIImage(cgImage: cgImg)
+      }
+    }
+    return UIImage(systemName: "xmark.circle") ?? UIImage()
+  }
 }
 
 struct MeView_Previews: PreviewProvider {
-    static var previews: some View {
-        MeView()
-    }
+  static var previews: some View {
+    MeView()
+  }
 }
