420c86cc99a6b942a90549a250906ffed03c5ac2
keisme <1822119568@qq.com>
2020-05-26 21:53:09 +0800

    update HotProspects

--- 100DaysOfSwiftUI/HotProspects/HotProspects/Prospect.swift
+++ 100DaysOfSwiftUI/HotProspects/HotProspects/Prospect.swift
@@ -9,16 +9,41 @@
 import Foundation
 
 class Prospect: Identifiable, Codable {
-    let id = UUID()
-    var name = "Anonymous"
-    var emailAddress = ""
-    var isContacted = false
+  let id = UUID()
+  var name = "Anonymous"
+  var emailAddress = ""
+  fileprivate(set) var isContacted = false
 }
 
 class Prospects: ObservableObject {
-    @Published var people: [Prospect]
-
-    init() {
-        self.people = []
+  static let saveKey = "SavedData"
+  @Published private(set) var people: [Prospect]
+  
+  init() {
+    if let data = UserDefaults.standard.data(forKey: Self.saveKey) {
+      if let decoded = try? JSONDecoder().decode([Prospect].self, from: data) {
+        self.people = decoded
+        return
+      }
     }
+    
+    self.people = []
+  }
+  
+  private func save() {
+    if let encoded = try? JSONEncoder().encode(people) {
+      UserDefaults.standard.set(encoded, forKey: Self.saveKey)
+    }
+  }
+  
+  func toggle(_ prospect: Prospect) {
+    objectWillChange.send()
+    prospect.isContacted.toggle()
+    save()
+  }
+  
+  func add(_ prospect: Prospect) {
+      people.append(prospect)
+      save()
+  }
 }
--- 100DaysOfSwiftUI/HotProspects/HotProspects/ProspectsView.swift
+++ 100DaysOfSwiftUI/HotProspects/HotProspects/ProspectsView.swift
@@ -8,6 +8,7 @@
 
 import SwiftUI
 import CodeScanner
+import UserNotifications
 
 enum FilterType {
   case none, contacted, uncontacted
@@ -48,9 +49,43 @@ struct ProspectsView: View {
       let person = Prospect()
       person.name = details[0]
       person.emailAddress = details[1]
-      self.prospects.people.append(person)
+      self.prospects.add(person)
     case .failure(let error):
       print("Scanning failed: \(error.localizedDescription)")
+    }
+  }
+  
+  func addNotification(for prospect: Prospect) {
+    let center = UNUserNotificationCenter.current()
+    
+    let addRequest = {
+      let content = UNMutableNotificationContent()
+      content.title = "Contact \(prospect.name)"
+      content.subtitle = prospect.emailAddress
+      content.sound = UNNotificationSound.default
+      
+//      var dateComponents = DateComponents()
+//      dateComponents.hour = 9
+//      let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
+      let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+      
+      let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+      center.add(request)
+    }
+    
+    // more code to come
+    center.getNotificationSettings { settings in
+      if settings.authorizationStatus == .authorized {
+        addRequest()
+      } else {
+        center.requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+          if success {
+            addRequest()
+          } else {
+            print("D'oh")
+          }
+        }
+      }
     }
   }
   
@@ -63,6 +98,16 @@ struct ProspectsView: View {
               .font(.headline)
             Text(prospect.emailAddress)
               .foregroundColor(.secondary)
+          }
+          .contextMenu {
+            Button(prospect.isContacted ? "Mark Uncontacted" : "Mark Contacted" ) {
+              self.prospects.toggle(prospect)
+            }
+            if !prospect.isContacted {
+              Button("Remind Me") {
+                self.addNotification(for: prospect)
+              }
+            }
           }
         }
       }
