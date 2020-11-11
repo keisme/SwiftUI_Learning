36ae1a19cc5d8e6903b4894cd5b6a0b0d6a91d68
keisme <1822119568@qq.com>
2020-11-11 14:59:05 +0800

    add GeometryReader

---
+++ GeometryReader/GeometryReader/GeometryReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometryReaderApp.swift
+//  GeometryReader
+//
+//  Created by Bruce on 2020/11/11.
+//
+
+import SwiftUI
+
+@main
+struct GeometryReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
